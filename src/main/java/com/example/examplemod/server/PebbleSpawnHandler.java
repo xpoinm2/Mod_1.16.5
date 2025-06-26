d1a5b7bdc5859b9d640d1d59d21b368033101991
xpoinm2 <_@example.com>
2025-06-26 03:07:20 +0300

    Изменить спавн камешков

--- src/main/java/com/example/examplemod/server/PebbleSpawnHandler.java
+++ src/main/java/com/example/examplemod/server/PebbleSpawnHandler.java
@@ -2,10 +2,11 @@ package com.example.examplemod.server;
 
 import com.example.examplemod.ExampleMod;
 import com.example.examplemod.ModItems;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
@@ -28,11 +29,10 @@ public class PebbleSpawnHandler {
 
         int x = chunk.getPos().getMinBlockX() + world.random.nextInt(16);
         int z = chunk.getPos().getMinBlockZ() + world.random.nextInt(16);
-        BlockPos pos = new BlockPos(x, 0, z);
-        Biome.Category category = world.getBiome(pos).getBiomeCategory();
-        if (category == Biome.Category.FOREST || category == Biome.Category.JUNGLE ||
-                category == Biome.Category.SWAMP || category == Biome.Category.ICY) {
-            int y = world.getHeight(Heightmap.Type.WORLD_SURFACE, x, z);
+        int y = world.getHeight(Heightmap.Type.WORLD_SURFACE, x, z);
+        BlockPos ground = new BlockPos(x, y - 1, z);
+        BlockState state = world.getBlockState(ground);
+        if (state.is(Blocks.GRASS_BLOCK)) {
             ItemEntity entity = new ItemEntity(world, x + 0.5, y + 0.0625, z + 0.5,
                     new ItemStack(ModItems.PEBBLE.get()));
             entity.setNoGravity(true);
