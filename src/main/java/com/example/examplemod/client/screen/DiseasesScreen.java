2406014b909b8380c7251a7d6d1ecd23923fe8d2
xpoinm2 <_@example.com>
2025-06-28 22:26:39 +0300

    Проверить механику болезни 'Вирусы'

--- src/main/java/com/example/examplemod/client/screen/DiseasesScreen.java
+++ src/main/java/com/example/examplemod/client/screen/DiseasesScreen.java
@@ -47,6 +47,8 @@ public class DiseasesScreen extends Screen {
             this.font.draw(ms, txt, (x0 + 10) * inv, (y0 + 40) * inv, 0xFFFFFF);
             String txt2 = String.format("переохлаждение - %d%%", stats.getHypothermia());
             this.font.draw(ms, txt2, (x0 + 10) * inv, (y0 + 55) * inv, 0xFFFFFF);
+            String txt3 = String.format("вирусы - %d%%", stats.getVirus());
+            this.font.draw(ms, txt3, (x0 + 10) * inv, (y0 + 70) * inv, 0xFFFFFF);
             ms.popPose();
         });
 
