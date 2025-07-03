e13391f23ed090953ad16c374b909a61b1fafeb5
xpoinm2 <_@example.com>
2025-07-03 16:11:08 +0300

    Исправить механику с лён в воде

--- src/main/java/com/example/examplemod/server/FlaxSoakHandler.java
+++ src/main/java/com/example/examplemod/server/FlaxSoakHandler.java
@@ -7,6 +7,9 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.common.ForgeMod;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,17 +18,23 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class FlaxSoakHandler {
     @SubscribeEvent
-    public static void onUse(PlayerInteractEvent.RightClickBlock event) {
+    public static void onUse(PlayerInteractEvent.RightClickItem event) {
         ItemStack stack = event.getItemStack();
         if (stack.getItem() != ModItems.FLAX.get()) return;
 
+        PlayerEntity player = event.getPlayer();
         World world = event.getWorld();
         if (world.isClientSide) return;
 
-        BlockPos pos = event.getPos();
+        double reach = player.getAttribute(ForgeMod.REACH_DISTANCE.get()) != null
+                ? player.getAttributeValue(ForgeMod.REACH_DISTANCE.get())
+                : 5.0D;
+        RayTraceResult res = player.pick(reach, 0.0F, true);
+        if (res.getType() != RayTraceResult.Type.BLOCK) return;
+
+        BlockPos pos = ((BlockRayTraceResult) res).getBlockPos();
         if (!world.getFluidState(pos).is(FluidTags.WATER)) return;
 
-        PlayerEntity player = event.getPlayer();
         Hand hand = event.getHand();
 
         ItemStack soaked = new ItemStack(ModItems.SOAKED_FLAX.get());
