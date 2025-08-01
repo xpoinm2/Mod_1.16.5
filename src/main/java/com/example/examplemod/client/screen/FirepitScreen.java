dad1266a1a086a27391fb4dbc521debb8ed7a46c
xpoinm2 <_@example.com>
2025-08-01 16:19:48 +0300

    Create standard interface with 12 slots

--- src/main/java/com/example/examplemod/client/screen/FirepitScreen.java
+++ src/main/java/com/example/examplemod/client/screen/FirepitScreen.java
@@ -3,6 +3,7 @@ package com.example.examplemod.client.screen;
 
 import com.example.examplemod.container.FirepitContainer;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
@@ -10,6 +11,8 @@ import net.minecraft.util.text.ITextComponent;
 
 
 public class FirepitScreen extends ContainerScreen<FirepitContainer> {
+    private static final ResourceLocation TEXTURE =
+            new ResourceLocation("minecraft", "textures/gui/container/generic_54.png");
 
     private final ITextComponent playerInventoryTitle;
 
@@ -30,14 +33,15 @@ public class FirepitScreen extends ContainerScreen<FirepitContainer> {
 
     @Override
     protected void renderLabels(MatrixStack ms, int mouseX, int mouseY) {
-        // Draw the title in red and center it
-        int titleX = (this.imageWidth - this.font.width(this.title)) / 2;
-        this.font.draw(ms, this.title, (float) titleX, 6.0F, 0xFF0000);
-        this.font.draw(ms, this.playerInventoryTitle, 8.0F, (float) (this.imageHeight - 96 + 2), 4210752);
+        this.font.draw(ms, this.title, 8.0F, 6.0F, 4210752);
+        this.font.draw(ms, this.playerInventoryTitle, 8.0F,
+                (float) (this.imageHeight - 96 + 2), 4210752);
     }
 
     @Override
     protected void renderBg(MatrixStack ms, float partialTicks, int x, int y) {
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.minecraft.getTextureManager().bind(TEXTURE);
         int i = this.leftPos;
         int j = this.topPos;
         blit(ms, i, j, 0, 0, this.imageWidth, this.imageHeight);
