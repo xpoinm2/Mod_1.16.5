7ca395d1387ffab5240f5a15f7f00ae6b7cb3bd2
xpoinm2 <_@example.com>
2025-10-11 23:41:04 +0300

    Создать пустой горшок с тонкими стенками

--- src/main/java/com/example/examplemod/block/RawClayPotBlock.java
+++ src/main/java/com/example/examplemod/block/RawClayPotBlock.java
@@ -12,13 +12,11 @@ import net.minecraft.world.IBlockReader;
 
 public class RawClayPotBlock extends Block {
     private static final VoxelShape SHAPE = VoxelShapes.or(
-            Block.box(6.0D, 0.0D, 6.0D, 10.0D, 1.0D, 10.0D),
-            Block.box(6.0D, 1.0D, 6.0D, 10.0D, 2.0D, 10.0D),
-            Block.box(5.0D, 2.0D, 5.0D, 11.0D, 3.0D, 11.0D),
-            Block.box(4.0D, 3.0D, 4.0D, 12.0D, 5.0D, 12.0D),
-            Block.box(4.0D, 5.0D, 4.0D, 12.0D, 7.0D, 12.0D),
-            Block.box(5.0D, 7.0D, 5.0D, 11.0D, 8.0D, 11.0D),
-            Block.box(4.0D, 7.75D, 4.0D, 12.0D, 8.0D, 12.0D)
+            Block.box(7.0D, 0.0D, 7.0D, 9.0D, 1.0D, 9.0D),
+            Block.box(6.0D, 1.0D, 6.0D, 10.0D, 3.0D, 10.0D),
+            Block.box(5.0D, 3.0D, 5.0D, 11.0D, 5.0D, 11.0D),
+            Block.box(6.0D, 5.0D, 6.0D, 10.0D, 6.5D, 10.0D),
+            Block.box(5.0D, 6.5D, 5.0D, 11.0D, 7.5D, 11.0D)
     );
 
     public RawClayPotBlock() {
@@ -32,10 +30,12 @@ public class RawClayPotBlock extends Block {
     public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext ctx) {
         return SHAPE;
     }
+
     @Override
     public VoxelShape getCollisionShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext ctx) {
         return SHAPE;
     }
+
     @Override
     public boolean propagatesSkylightDown(BlockState state, IBlockReader world, BlockPos pos) {
         return true;
