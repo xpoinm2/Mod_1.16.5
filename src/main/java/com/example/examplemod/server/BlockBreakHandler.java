04a89af246f018406a2e9ae98bb87cf5797a526f
xpoinm2 <_@example.com>
2025-12-04 19:35:34 +0300

    Remove deepslate variants from the set of vanilla ores in BlockBreakHandler to streamline block breaking logic and improve code clarity.

--- src/main/java/com/example/examplemod/server/BlockBreakHandler.java
+++ src/main/java/com/example/examplemod/server/BlockBreakHandler.java
@@ -36,21 +36,12 @@ public class BlockBreakHandler {
     private static final String KEY_THIRST = "thirst";
     private static final Set<Block> VANILLA_ORES = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
             Blocks.COAL_ORE,
-            Blocks.DEEPSLATE_COAL_ORE,
             Blocks.IRON_ORE,
-            Blocks.DEEPSLATE_IRON_ORE,
             Blocks.GOLD_ORE,
-            Blocks.DEEPSLATE_GOLD_ORE,
-            Blocks.COPPER_ORE,
-            Blocks.DEEPSLATE_COPPER_ORE,
             Blocks.REDSTONE_ORE,
-            Blocks.DEEPSLATE_REDSTONE_ORE,
             Blocks.LAPIS_ORE,
-            Blocks.DEEPSLATE_LAPIS_ORE,
             Blocks.DIAMOND_ORE,
-            Blocks.DEEPSLATE_DIAMOND_ORE,
             Blocks.EMERALD_ORE,
-            Blocks.DEEPSLATE_EMERALD_ORE,
             Blocks.NETHER_QUARTZ_ORE,
             Blocks.NETHER_GOLD_ORE,
             Blocks.ANCIENT_DEBRIS
