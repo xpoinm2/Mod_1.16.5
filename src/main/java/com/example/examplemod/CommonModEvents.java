7f3a361e1aa1f0aa5a3eb203ae636c7f639d58c0
xpoinm2 <_@example.com>
2025-12-26 16:10:31 +0300

    Remove Beaver entity attribute registration from CommonModEvents
    - Deleted the onAttributes method for BeaverEntity to streamline event handling.
    - Continued cleanup following the removal of Beaver-related assets from the mod.

--- src/main/java/com/example/examplemod/CommonModEvents.java
+++ src/main/java/com/example/examplemod/CommonModEvents.java
@@ -1,21 +1,14 @@
 package com.example.examplemod;
 
-import com.example.examplemod.entity.BeaverEntity;
 import com.example.examplemod.world.WorldGenRegistry;
 import com.example.examplemod.world.ModBiomes;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class CommonModEvents {
     private CommonModEvents() {
-    }
-
-    @SubscribeEvent
-    public static void onAttributes(EntityAttributeCreationEvent event) {
-        event.put(ModEntities.BEAVER.get(), BeaverEntity.createAttributes().build());
     }
 
     @SubscribeEvent
