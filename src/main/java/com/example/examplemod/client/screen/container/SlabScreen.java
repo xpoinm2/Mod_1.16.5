db5f4749d9a8f870b11c841f5d0496974887dc42
xpoinm2 <_@example.com>
2025-12-27 18:50:49 +0300

    Update SlabScreen to improve GUI rendering and texture handling
    - Replaced CONTAINER_TEXTURE with CRAFTING_TABLE_TEXTURE for better visual consistency.
    - Enhanced the renderBg method to programmatically draw the GUI background and borders, improving the overall appearance.
    - Added detailed comments to clarify the rendering process and maintainability of the code.

--- src/main/java/com/example/examplemod/client/screen/container/SlabScreen.java
+++ src/main/java/com/example/examplemod/client/screen/container/SlabScreen.java
@@ -3,14 +3,15 @@ package com.example.examplemod.client.screen.container;
 import com.example.examplemod.container.SlabContainer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 
 public class SlabScreen extends ContainerScreen<SlabContainer> {
-    private static final ResourceLocation CONTAINER_TEXTURE =
-            new ResourceLocation("minecraft", "textures/gui/container/generic_54.png");
+    private static final ResourceLocation CRAFTING_TABLE_TEXTURE =
+            new ResourceLocation("minecraft", "textures/gui/container/crafting_table.png");
 
     public SlabScreen(SlabContainer screenContainer, PlayerInventory inv, ITextComponent title) {
         super(screenContainer, inv, title);
@@ -25,16 +26,39 @@ public class SlabScreen extends ContainerScreen<SlabContainer> {
     @Override
     protected void renderBg(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY) {
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.minecraft.getTextureManager().bind(CONTAINER_TEXTURE);
         
-        // Рисуем серый фон верхней части (фон для контейнера без нарисованных слотов)
-        blit(matrixStack, this.leftPos, this.topPos, 0, 0, 176, 54, 256, 256);
+        // Рисуем весь фон программно без нарисованных слотов на текстуре
+        // Цвет фона как у ванильного GUI: #C6C6C6 (198, 198, 198)
+        AbstractGui.fill(matrixStack, this.leftPos, this.topPos, this.leftPos + this.imageWidth, this.topPos + this.imageHeight, 0xFFC6C6C6);
         
-        // Рисуем серый фон нижней части (инвентарь игрока)
-        blit(matrixStack, this.leftPos, this.topPos + 54, 0, 126, 176, 97, 256, 256);
+        // Рисуем рамку всего GUI в стиле ванильного GUI
+        // Внешняя темная рамка
+        AbstractGui.fill(matrixStack, this.leftPos, this.topPos, this.leftPos + this.imageWidth, this.topPos + 1, 0xFF000000);
+        AbstractGui.fill(matrixStack, this.leftPos, this.topPos + this.imageHeight - 1, this.leftPos + this.imageWidth, this.topPos + this.imageHeight, 0xFF000000);
+        AbstractGui.fill(matrixStack, this.leftPos, this.topPos, this.leftPos + 1, this.topPos + this.imageHeight, 0xFF000000);
+        AbstractGui.fill(matrixStack, this.leftPos + this.imageWidth - 1, this.topPos, this.leftPos + this.imageWidth, this.topPos + this.imageHeight, 0xFF000000);
         
-        // Окоемки слотов рисуются автоматически системой Minecraft для каждого слота
-        // Нам нужно только обеспечить правильный серый фон
+        // Внутренняя светлая рамка (верхняя и левая)
+        AbstractGui.fill(matrixStack, this.leftPos + 1, this.topPos + 1, this.leftPos + this.imageWidth - 1, this.topPos + 2, 0xFF8B8B8B);
+        AbstractGui.fill(matrixStack, this.leftPos + 1, this.topPos + 1, this.leftPos + 2, this.topPos + this.imageHeight - 1, 0xFF8B8B8B);
+        
+        // Внутренняя темная рамка (нижняя и правая)
+        AbstractGui.fill(matrixStack, this.leftPos + 1, this.topPos + this.imageHeight - 2, this.leftPos + this.imageWidth - 1, this.topPos + this.imageHeight - 1, 0xFF373737);
+        AbstractGui.fill(matrixStack, this.leftPos + this.imageWidth - 2, this.topPos + 1, this.leftPos + this.imageWidth - 1, this.topPos + this.imageHeight - 1, 0xFF373737);
+        
+        // Разделительная линия между контейнером и инвентарём игрока
+        AbstractGui.fill(matrixStack, this.leftPos + 1, this.topPos + 54, this.leftPos + this.imageWidth - 1, this.topPos + 54, 0xFF373737);
+        AbstractGui.fill(matrixStack, this.leftPos + 1, this.topPos + 55, this.leftPos + this.imageWidth - 1, this.topPos + 55, 0xFF8B8B8B);
+        
+        // Рисуем окоёмку для сетки 3x3 в стиле ванильного верстака
+        // Используем текстуру верстака для окоёмки сетки
+        // В ванильном верстаке окоёмка сетки находится на координатах (7, 17) размером 58x58
+        // Наша сетка начинается с (62, 17), поэтому сдвигаем окоёмку на правильную позицию
+        this.minecraft.getTextureManager().bind(CRAFTING_TABLE_TEXTURE);
+        int frameX = this.leftPos + SlabContainer.GRID_START_X - 1;
+        int frameY = this.topPos + SlabContainer.GRID_START_Y - 1;
+        // Координаты окоёмки на текстуре верстака: (7, 17) размер 58x58 (включая рамку вокруг сетки 3x3)
+        blit(matrixStack, frameX, frameY, 7, 17, 58, 58, 256, 256);
     }
 
     @Override
